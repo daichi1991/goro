7c5f9784a971913394773f16ae848501f45e576f
daichi1991 <ks-pan@live.jp>
2021-05-03 23:22:03 +0900

    jwt in cookie

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < Devise::SessionsController
+
+    def create
+        user = User.find_by_email(sign_in_params[:email])
+
+        if user && user.valid_password?(sign_in_params[:password])
+            token = current_user.generate_jwt
+            cookies[:token]= {value:token, httponly: true}
+            render json: token.to_json
+        else
+            render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+        end
+    end
+
+end
