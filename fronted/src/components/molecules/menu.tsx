303116ce676d262b434dc02d97bbda5d97be51b7
daichi1991 <ks-pan@live.jp>
2022-01-09 16:07:48 +0900

    rm -rf fronted

--- fronted/src/components/molecules/menu.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const Menu:React.FC=() =>{
-    return(
-        <>メニュー</>
-    )
-}
--- fronted/src/components/molecules/user/userSignUpSentMail.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const UserSignUpSendMail:React.FC = () =>{
-    return (
-        <h1>認証メールを送信しました</h1>
-    )
-}
