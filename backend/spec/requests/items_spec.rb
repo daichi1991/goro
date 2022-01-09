07b9599ed1ecee45229d606deed2c52dddce0165
daichi1991 <ks-pan@live.jp>
2022-01-09 13:54:43 +0900

    RspecでPOST itemsのテストを追加

--- backend/spec/requests/items_spec.rb
+++ backend/spec/requests/items_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe "Items", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /api/v1/items.json" do
+    let(:user) { User.create }
+    let(:token) { user.generate_jwt }
+
+    it "creates item collectly" do
+      expect {
+        post '/api/v1/items.json', params: {
+          item: {
+            title: "江戸幕府が開かれる",
+            year: "3",
+            year_type: "紀元前",
+            goro_text: "ヒーローのおっさん徳川家康登場",
+            description: "aaa",
+          }
+        }, headers: { cookie: "token=#{token}" }  
+      }.to change(Item, :count).by(1)
+      expect(Item.last.year_for_sort).to eq -3
+      expect(response).to have_http_status(200)
+    end
+  end
 end
